b7e8d4b8b1fa958d64b7d77d881d0534349844bf
othniel2471 <othnielomaidu@gmail.com>
2023-07-13 23:58:28 +0100

    Completed setup for project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Categories from './pages/Categories';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
       </Routes>
     </Router>
   );
